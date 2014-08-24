c9e983d0c3cc5d6d5a1dabddc5824a1af7e0368d
koheishingai <koheishingai@gmail.com>
2014-08-24 16:42:46 +0900

    add something

--- reader/routes/index.js
+++ reader/routes/index.js
@@ -3,3 +3,9 @@ var io = require("../../node_modules/socket.io").listen(8080);
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+io.sockets.on("connection", function (socket) {
+  socket.on("read", function(url){
+    console.log(url);
+  });
+});
